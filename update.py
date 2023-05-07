d3e2716974bed5852aa68bd75502cedd88eab7ff
telugubhasha <111115839+telugubhasha@users.noreply.github.com>
2023-05-07 11:03:25 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
-from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_inf
+from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
 from os import path as ospath, environ
 from subprocess import run as srun
 from dotenv import load_dotenv
